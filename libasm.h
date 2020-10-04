c49d6432319c00b2ad167d9628c96adbacccd169
Sceletoshka <sceletoshka@gmail.com>
2020-10-04 20:43:19 +0300

    libasm.h

--- libasm.h
+++ libasm.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libasm.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smatha <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: smatha <smatha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/03 14:12:55 by smatha            #+#    #+#             */
-/*   Updated: 2020/10/03 14:30:37 by smatha           ###   ########.fr       */
+/*   Updated: 2020/10/04 20:42:58 by smatha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@
 #include <stdlib.h>
 
 size_t		ft_strlen(const char *str);
-char		*ft_strcpy(char *dst, const char *src);
+char		*ft_strcpy(char *dest, const char *s);
 int			ft_strcmp(const char *s1, const char *s2);
 ssize_t		ft_write(int fd, const void *buff, size_t byte);
 ssize_t		ft_read(int fd, void *buff, size_t byte);
-char		*ft_strdup(const char *s1);
+char		*ft_strdup(const char *s);
 
-#endif 
+#endif
