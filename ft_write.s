77bef8762ec5d914abe512a5a6de48f194c8ed8d
Sueann Matha <smatha@ga-u3.msk.21-school.ru>
2020-10-09 15:17:39 +0300

    ft_read.s

--- ft_write.s
+++ ft_write.s
@@ -9,6 +9,16 @@ section .text
 	global _ft_write
 
 _ft_write:
+	mov r8, rdx				; save rdx = len in r8
 	mov rax, 0x2000004
 	syscall
+		jc exit_error		; if doesn't work, write set flags carry to 1 so jmp exit error
+	jmp exit
+
+exit_error:
+	mov rax, -1				; set return to -1
+	ret
+
+exit:
+	mov rax, r8				; set previous value of rdx save in r8, in return value
 	ret
