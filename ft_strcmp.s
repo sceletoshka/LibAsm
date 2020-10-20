a0cd9ad1880eb36ce8444319196a5f15ac5818fb
Sueann Matha <smatha@ga-u5.msk.21-school.ru>
2020-10-20 22:29:20 +0300

    ft_read.s

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -7,20 +7,20 @@ section .text
 
 _ft_strcmp:
 	mov 	rax, 0
-	jmp		compa
+	jmp		compare
 
-compa:
+compare:
 	mov		al, BYTE [rdi]	; get the least significant byte in rdi where is stock the value of the char
 	mov		bl, BYTE [rsi]	; get the least significant byte in rsi where is stock the value of the char
 	cmp		al, 0			; if we are not at the end of rdi (arg0)
 	je		exit
 	cmp		bl, 0			; if we are not at the end of rdi (arg1)
 	je		exit
-	cmp 	al, bl			; compare al and bl
+	cmp 	al, bl
 	jne 	exit
 	inc 	rdi
 	inc 	rsi
-	jmp 	compa
+	jmp 	compare
 
 exit:
 	movzx	rax, al
