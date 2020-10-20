c4a9008ce13885260325fa0574e9d981cc16866e
Sueann Matha <smatha@ga-u5.msk.21-school.ru>
2020-10-20 19:34:20 +0300

    ft_read.s

--- ft_read.s
+++ ft_read.s
@@ -9,17 +9,21 @@
 
 section .text
 	global _ft_read
+	extern ___error
 
 ; delete all unpreserved registers by _read
 
 _ft_read:								; fd = rdi, buffer = rsi, bytes = rdx
 	mov		rax, 0x2000003
 	syscall
-			jc exit_error		; if doesn't work, read set flags carry to 1 so jmp exit error
+			jc _error
 	jmp exit
 
-exit_error:
-	mov 	rax, -1			; set return to -1s
+_error:
+	mov r15, rax
+	call ___error
+	mov [rax], r15
+	mov rax, -1
 	ret
 	
 exit:
