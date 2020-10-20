1b28d14dde74093766e7729eaa110c603d196f16
Sueann Matha <smatha@ga-u5.msk.21-school.ru>
2020-10-20 23:28:13 +0300

    ft_read.s

--- ft_read.s
+++ ft_read.s
@@ -11,8 +11,6 @@ section .text
 	global _ft_read
 	extern ___error
 
-; delete all unpreserved registers by _read
-
 _ft_read:								; fd = rdi, buffer = rsi, bytes = rdx
 	mov		rax, 0x2000003
 	syscall
